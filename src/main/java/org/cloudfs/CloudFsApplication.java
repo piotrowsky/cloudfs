bacba1cebd11c5d06557ec94b8cc314b41ab42af
p1ter <pzdzisiu@gmail.com>
2016-11-02 19:07:58 +0100

    Example spring boot app

---
+++ src/main/java/org/cloudfs/CloudFsApplication.java
@@ -0,0 +1,12 @@
+package org.cloudfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFsApplication.class, args);
+	}
+}
